925a495f6a340fb5ffe84d703618bea39253ff9a
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-04-17 08:54:26 +0530

    live agent api call

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 import path from 'path';
 import indexRouter from './routes/index';
 import { chatResponse } from './controllers/chatController';
+import { liveChat } from './controllers/liveChatController';
 import "dotenv/config";
 import bodyParser from 'body-parser';
 import { viewDocuments } from './controllers/viewDocumentsController';
@@ -59,6 +60,7 @@ app.use(flash());
 // Routes
 app.use('/', indexRouter);
 app.post('/api/chat-response', chatResponse);
+app.post('/live-chat-agent', liveChat);
 app.get('/view-documents', adminLogged, viewDocuments);
 app.get('/upload-documents', adminLogged, (req: Request, res: Response) => {
     res.render('upload-documents');
