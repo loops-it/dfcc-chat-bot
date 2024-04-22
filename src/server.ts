2af1a1951289f00a7c208db1c4f57ab5a093ddd2
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-04-22 16:19:38 +0530

    webhook query test

--- src/server.ts
+++ src/server.ts
@@ -299,7 +299,7 @@ app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
-
+  console.log(req.query);
   if (mode && token) {
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       console.log('WEBHOOK_VERIFIED');
