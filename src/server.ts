43c055f88a82cde99cb44b48b392893fe5a57abb
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-04-22 16:11:59 +0530

    webhook api

--- src/server.ts
+++ src/server.ts
@@ -293,6 +293,22 @@ app.post("/refresh-live-chat-inner",refreshLiveChatInner)
 
 
 
+app.get('/webhook', (req, res) => {
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
