a6615b1a7e39627575baa1c9e46f3f886091e50a
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-05-20 17:50:35 +0530

    Facebook Chat with intent 9

--- src/controllers/chatControllerFacebook.ts
+++ src/controllers/chatControllerFacebook.ts
@@ -297,7 +297,7 @@ export const chatControllerFacebook = async (req: RequestWithChatId, res: Respon
             const intentToSend = stateProduct.trim().toLowerCase();
 
 try {
-    let data: any[] = [];
+    let intentData: any[] = [];
     const node_details = await Node.findAll({
         where: {
             intent: intentToSend,
@@ -307,11 +307,11 @@ try {
     for (const node of node_details) {
         const { type, node_id } = node;
         let nodeData;
-        let data;
+        let message_data;
         switch (type) {
             case 'textOnly':
                 nodeData = await FlowTextOnly.findOne({ where: { node_id } });
-                data = {
+                message_data = {
                     recipient: {
                         id: message_body.sender.id
                     },
@@ -323,7 +323,7 @@ try {
                 break;
             case 'textinput':
                 nodeData = await FlowTextBox.findOne({ where: { node_id } });
-                data = {
+                message_data = {
                     recipient: {
                         id: message_body.sender.id
                     },
@@ -346,10 +346,11 @@ try {
                 break;
             case 'cardStyleOne':
                 nodeData = await FlowCardData.findOne({ where: { node_id } });
-                data = {
+                message_data = {
                     recipient: {
                         id: message_body.sender.id
                     },
+                    messaging_type: "RESPONSE",
                     message: {
                         attachment: {
                             type: "template",
@@ -374,7 +375,7 @@ try {
                     title: button.text,
                     payload: button.link
                 }));
-                data = {
+                message_data = {
                     recipient: {
                         id: message_body.sender.id
                     },
@@ -418,7 +419,7 @@ try {
                         }
                     }
                 }));
-                data = {
+                message_data = {
                     recipient: {
                         id: message_body.sender.id,
                     },
@@ -441,14 +442,18 @@ try {
             default:
                 continue;
         }
-         // Add each message_data to intentData array
+        intentData.push(message_data);  // Add each message_data to intentData array
     }
-        console.log("template data :",data);
+
+    // Send all collected messages
+    for (const data of intentData) {
+        console.log(data);
         await axios.post(`https://graph.facebook.com/v19.0/me/messages?access_token=EAAF348C6zRwBOygEAVOQDjd3QK5YhIHbGGmdDDca0HDaDEbS0sdlEqPycuP7satY9GPf6QPhYTVdUawRe7XTZBAQkaAT6rPrqNVICUNjcYxuZApRs6YjzUYpqxzUtbW1lUSyN2z4VhLhMAeMmiCzYtawEStMYtZCNIZBcOeEIB0glhiTRkT0qaXuB9I0m3Dd`, data, {
             headers: {
                 'Content-Type': 'application/json'
             }
         });
+    }
 
     res.json({ status: "success" });
 
@@ -458,6 +463,7 @@ try {
 }
 
         }
+        
 
     } catch (error) {
         console.error("Error processing question:", error);
